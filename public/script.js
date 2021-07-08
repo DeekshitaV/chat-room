2c681d399aa0ef9b9e101fe814a8de5b19adf9dc
DeekshitaV <deekshitaverma@gmail.com>
2021-07-08 19:40:10 +0530

    chatRoom

--- public/script.js
+++ public/script.js
@@ -1,47 +1,66 @@
-const socket = io('/');
+const socket = io("/");
 
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
 var username = "";
-do{
-  username = prompt('Enter your display name');
-}while(!username);
+do {
+    username = prompt('Enter your display name');
+} while (!username);
 const user = username;
+
+firebase.database().ref(ROOM_ID + '/messages').once( 'value' , (snapshot) => {
+  snapshot.forEach(element => {
+      var obj = element.val();
+      messages.innerHTML =
+      messages.innerHTML +
+      `<div class="message">
+          <b><i class="far fa-user-circle"></i> <span> ${
+              obj.name
+          }</span> </b>
+          <span>${obj.message}</span>
+      </div>`; 
+  });
+  scrollToBottom();
+}) 
 
 let text = document.querySelector('#chat-message');
 let send = document.getElementById('send');
 let messages = document.querySelector('.messages');
 
 const sendData = (textMessage, userName) => {
-  firebase.database().ref( ROOM_ID + '/messages').push({ name : userName , message : textMessage});
+    firebase.database().ref(ROOM_ID + '/messages').push({ name: userName, message: textMessage });
 }
 
 const scrollToBottom = () => {
-        
-  let d = $('.main-chat-window');
-  d.scrollTop(d.prop('scrollHeight'));
+
+    let d = $('.main-chat-window');
+    d.scrollTop(d.prop('scrollHeight'));
 }
 
-send.addEventListener("click" , (e) => {
-    if(text.value.length !== 0 ){
-        socket.emit("chat" , (ROOM_ID, text.value , user) );
-        sendData(text.value,user);     
+send.addEventListener("click", (e) => {
+    if (text.value.length !== 0) {
+
+        socket.emit("chat", ROOM_ID, text.value, user);
+        console.log(ROOM_ID, text.value, user);
+        sendData(text.value, user);
         text.value = "";
-        
+
     }
- });
- 
- text.addEventListener("keydown" , (e) => {
-     if(e.key === "Enter" && text.value.length !== 0 ){
-         socket.emit("chat" , (ROOM_ID, text.value , user) );
-         sendData(text.value,user);
-         text.value = "";            
-     }
- });
- 
+});
+
+text.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && text.value.length !== 0) {
+      console.log(ROOM_ID, text.value, user);
+        socket.emit("chat", ROOM_ID, text.value, user);
+        sendData(text.value, user);
+        text.value = "";
+
+    }
+});
+
  socket.on("create-message", (message, userName) => {
-   console.log('received');
+  console.log(message, userName);
   messages.innerHTML =
     messages.innerHTML +
     `<div class="message">
--- server.js
+++ server.js
@@ -2,25 +2,34 @@ const express = require('express');
 
 const app = express();
 const server = require('http').Server(app);
-const { v4 : uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const io = require('socket.io')(server);
 app.set('view engine', 'ejs');
 const path = require('path');
 app.use(express.static(path.join(__dirname + '/public')));
 
-app.get('/' , (req,res) => {
-   res.redirect(`${uuidv4()}`);
+app.get('/', (req, res) => {
+    res.redirect(`${uuidv4()}`);
 })
 
-app.get('/:room' , (req,res) => {
-    res.render("ChatRoom" , {roomId: req.params.room});
+app.get('/:room', (req, res) => {
+    res.render("ChatRoom", { roomId: req.params.room });
 })
 
-io.on('connection' , (socket) => {
-    socket.on("chat", ( roomId, message , userName) => {
-        console.log("sent");
-       io.to(roomId).emit('create-message' , ( message , userName));
-    }); 
+var users = {};
+
+
+io.on("connection", (socket) => {
+    console.log("userConnected");
+    socket.on("chat", ( roomId , message , userName) => {
+      console.log(roomId, message , userName );
+        users[userName] = roomId;
+        socket.join(roomId);
+        console.log(users);
+        console.log("chat joined");
+        io.to(roomId).emit("create-message", message,userName );
+        
+    })
 })
 
-server.listen( process.env.PORT || 3000);
+server.listen(process.env.PORT || 3000);
