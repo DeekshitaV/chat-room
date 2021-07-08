3e551731078f6d524968caeeb16c6b9ece8efd07
DeekshitaV <deekshitaverma@gmail.com>
2021-07-08 15:46:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/:room' , (req,res) => {
 io.on('connection' , (socket) => {
     socket.on("chat", ( roomId, message , userName) => {
         console.log("sent");
-       io.to(roomId).emit('chat-message' , ( message , userName));
+       io.to(roomId).emit('create-message' , ( message , userName));
     }); 
 })
 
